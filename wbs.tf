ccc0f2085289a83ac2fca3a4d81e12842d7cfd09
Junaidul Haq <haqjunaidul5@gmail.com>
2022-11-08 14:58:07 +0530

    first

--- wbs.tf
+++ wbs.tf
@@ -1,8 +1,8 @@
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.medium"
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
@@ -15,8 +15,8 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.medium"
   availability_zone      = "us-east-1b"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-2.id
